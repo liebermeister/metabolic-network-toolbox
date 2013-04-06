8f3ef9be7e2de32b6ce72f0c6bb8af76951b6ab7
wolfram.liebermeister@gmail.com <wolfram@persephone.(none)>
2013-04-06 13:30:34 +0200

    changes in demo file

--- demo/mnt_demo.m
+++ demo/mnt_demo.m
@@ -1,156 +1,228 @@
-%MNT_DEMO - Demo for Metabolic Network Toolbox
 
 data_dir = [fileparts(which(mfilename)) '/data'];
 
 echo on;
 clc
-% We consider the following simple network
+%----------------------------------------------
+% MNT_DEMO - Demo for Metabolic Network Toolbox
 %
+% In this demo, a small metabolic network is 
+% built from its stoichiometric matrix, 
+% analysed by Metabolic Control Analysis, 
+% and a time simulation is run.
+%----------------------------------------------
+ 
+% Please press any key to continue
+ 
+pause
+clc
+%----------------------------------------------
+% We consider the following simple network
+ 
 % S1             S5
 %    \         /
 %      S3 - S4
 %    /         \
 % S2             S6
-%
-% (where Sn denotes metabolites) to illustrate some
-% of the functions of the Metabolic Network Toolbox.
-%
+ 
+% to illustrate some functions of the Metabolic Network Toolbox.
+ 
 % Please press any key to continue
-
+ 
 pause
 clc
-% We define the network by its stoichiometric matrix N,
-% the reversibility of reactions (bit string) 
-% and the external metabolites (indices)
-%
-
-N            = [ -1 0 0 0 0; ...
-		 0 -1 0 0 0; ...
-		 1 1 -1 0 0; ...
-		 0 0 1 -1 -1; ...
-		 0 0 0 1 0; ...
-		 0 0 0 0 1];
-
+%----------------------------------------------
+% We first need to define the network. 
+% This is done by the stoichiometric matrix N,
+% the reversibility of reactions (as a bit column),
+% and the indices of external metabolites
+ 
+N = [ -1  0  0  0  0; ...
+       0 -1  0  0  0; ...
+       1  1 -1  0  0; ...
+       0  0  1 -1 -1; ...
+       0  0  0  1  0; ...
+       0  0  0  0  1];
+ 
 reversible   = [1 1 1 1 1]';
+ 
+ind_external = [1 2 5 6 ]';
+ 
+network = network_construct(N, reversible, ind_external);
 
-external_ind = [1 2 5 6 ]';
-
-%
 % Press any key to continue
+ 
 pause
+clc
+%----------------------------------------------
+% All information about the network is now stored in the structure
+ 
+network
 
-% All information about the network is then stored in a matlab structure
-
-network      = network_construct(N,reversible,external_ind)
 % The fields 'kinetics' and 'graphics_par' contain information
 % about reaction kinetics and the graphical layout (set here by default).
-
+ 
+% The following command draws the network
+ 
+figure(1);
+netgraph_draw(network);
+ 
+% The arrows indicate the nominal reaction directions.
+ 
 % Press any key to continue
+ 
 pause
 clc
+%----------------------------------------------
+% Now we will analyse the network structure.
+% Information about stationary fluxes and conservation relations
+% can be obtained from the kernel matrix K and the link matrix L. 
+% Both follow from the stoichimetric matrix of internal metabolites.
 
-% Now we analyse the network structure
-% Information about stationary fluxes and conservation
-% relations can be obtained from the kerbel matrix K
-% and the link matrix L (for the stoichimetric matrix 
-% of internal metabolites)
-[ K, L ] = analyse_N(N(find(network.external==0),:))
+[K, L] = analyse_N(N(find(network.external==0),:))
+ 
 % Press any key to continue
+ 
 pause
 clc
+%----------------------------------------------
 % Until here, we only considered the network structure.
-% Now we define a kinetics.
-% There are different ways to specify a kinetics (see 'kinetics_structure'). 
-% Here we choose a common modular rate law kinetics for all reactions.
-%
+% Kinetic rate laws can be stored in the field 'network.kinetics'. 
+% There are different ways to specify them (see 'kinetics_structure'). 
+% This is how we choose common modular rate laws for all reactions.
+ 
 network.kinetics = set_kinetics(network,'cs');
-% This updates the field 'kinetics', which contains all kinetic data.
-%
+ 
 % Press any key to continue
+ 
 pause
 clc
+%----------------------------------------------
 % Now we can run dynamical simulations.
-% First, we compute a stationary state, starting from an initial guess
-% of the concentrations ...
-
+% First, we guess an intial set of concentrations ..
+ 
 s = [1 1 0 0 0 0]';
-
+ 
+% .. compute stationary concentrations and fluxes ..
+ 
 [S, J] = network_steady_state(network,s);
-
-% ... and plot it on the network graph. 
-%
-
+ 
+% .. and plot them on the network graph. 
+ 
 network = netgraph_make_graph(network);
+ 
+figure(2);
 netgraph_draw(network,{'metvalues',S,'arrowvalues',J,'arrowstyle','fluxes'});
-
-% The size (and color) of octagons 
-% and squares represents the values of concentrations and fluxes.
-%
+ 
+% The circles and squares represent metabolites and reactions.
+ 
 % Press any key to continue
+ 
 pause
 clc
-% Likewise, we can compute properties from metabolic control analysis,
-% for instance, elasticity coefficients and control coefficients
-% (for more information: type 'help control_theory')
-%
+%---------------------------------------------------------------------
+% Likewise, we can compute elasticity and control coefficients
+% (for information on metabolic control analysis, type 'help mnt_mca')
+ 
+epsilon     = elasticities(network,S);
+ 
+[C_J, C_S]  = control_coefficients(network.N,epsilon,network.external);
 
-epsilon       = elasticities(network,S);
-[ C_J, C_S ]  = control_coefficients(network.N,epsilon,network.external);
-
-% Here we plot the control coefficients on the 4th metabolite 
-
+% Here we plot the control coefficients on the 4th metabolite on the network
+ 
+figure(3);
 netgraph_draw(network,'actvalues',C_S(4,:)');
-
+ 
 % Press any key to continue
+ 
 pause
 clc
+%---------------------------------------------------------------------
 % Metabolic control theory has proven theorems that relate the 
 % control coefficients to the network structure (described by 
 % K and L) and the reaction elasticities.
-%
-% The theorems for non-normalised control coefficients read
-%
-%Summation theorems
-% CJ K         = K      
-% CS K         = 0
-%
-%Connectivity theorems
-% CJ epsilon L = 0
-% CS epsilon L = - L
-%
-%Let us check them:
-%
-% CJ K = K
+ 
+% Summation theorems
+ 
+%  CJ * K  = K      
+%  CS * K  = 0
+ 
+% Connectivity theorems
+ 
+%  CJ * epsilon * L = 0
+%  CS * epsilon * L = - L
+ 
+% We will now check if our control coefficients satisfy them
+ 
+% In the theorems, we consider only the elasticities and concentration 
+% control matrices with respect to internal metabolites 
+ 
+ind_internal = find(network.external==0);
+ 
+epsilon_int  = epsilon(:,ind_internal);
+ 
+C_S_int      = C_S(ind_internal,:);
 
-C_J*K
-K
-pause
-% CS K = 0
-C_S(find(network.external==0),:)*K
-pause
-% CJ epsilon L = 0
-C_J(find(network.external==0),:)*epsilon(:,find(network.external==0))*L
-pause
-% CS epsilon L = - L
-C_S(find(network.external==0),:)*epsilon(:,find(network.external==0))*L
-L
+% Press any key to continue
+ 
 pause
 clc
-% We compute a time course with initial conditions given by s:
+%---------------------------------------------------------------------
+% Since CJ K = K, the following should vanish:
+
+C_J * K - K
+ 
+% Press any key to continue
+ 
+pause
+clc
+%---------------------------------------------------------------------
+% Since CS K = 0, the following should vanish:
+ 
+C_S_int * K
+ 
+% Press any key to continue
+ 
+pause
+clc
+%---------------------------------------------------------------------
+% Since CJ epsilon L = 0, the following should vanish:
+ 
+C_J * epsilon_int * L
+ 
+% Press any key to continue
+ 
+pause
+clc
+%---------------------------------------------------------------------
+% Since CS epsilon L = - L, the following should vanish:
+
+C_S_int * epsilon_int * L + L
+ 
+% Press any key to continue
+ 
+pause
+clc
+%---------------------------------------------------------------------
+% Now we compute a time course, with initial conditions given by s:
 
 [t,s_t,s_int] = network_integrate(network,s,5);
-close
-plot(t,s_t)
+
+figure(4); plot(t,s_t)
 
 % Press any key to continue
 pause
 
+% --------------------------------------------------------------------
 % We can also show the time course as a movie
+ 
 M = netgraph_movie(network,t,s_t);
+ 
+figure(4); 
 movie(M,1);
-% Press any key to continue
+ 
+% Press any key to finish
 pause
-close
-
+ 
 % Enjoy this toolbox!
 echo off;
